16be9ccedd0f0eb5af3c368b9c66b9141eb84618
JordanWilliford21 <jordanwilliford@westernwyoming.edu>
2025-01-18 03:58:56 +0000

    Ready for Grading

--- hello-world-again/main.py
+++ hello-world-again/main.py
@@ -1,8 +1,16 @@
 #
-# Name
-# Date
+# Jordan
+# 1/17/2025
 # Hello Word! Again
 # SDEV 1200 
 #
 
 # Use comments liberally throughout the program.
+# This will define what greeting is and what it should say
+def greeting():
+    message = 'World'
+    print(f"Hello {message}")
+#This will define the main function of the code
+def main():
+    greeting()
+main()
