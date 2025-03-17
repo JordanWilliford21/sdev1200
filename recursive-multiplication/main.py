6ea58242e06fcea06f128dcc0e80ee7a13e7c8e2
JordanWilliford21 <jordanwilliford@westernwyoming.edu>
2025-03-17 02:14:44 +0000

    ready to grade

--- recursive-multiplication/main.py
+++ recursive-multiplication/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Jordan Williford
+# 3/16/2025
 # Recursive Multiplication Programming Project
 # SDEV 1200
 #
 # Use comments liberally throughout the program. 
+def recursive_multiply(x, y):
+    # Base case: If y is 1, return x
+    if y == 1:
+        return x
+    # Recursive case: Add x to the result of multiplying x and (y-1)
+    else:
+        return x + recursive_multiply(x, y - 1)
+
+# Example usage:
+result = recursive_multiply(7, 4)
+print(result)  # Output will be 28
