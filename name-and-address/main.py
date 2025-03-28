22cd3d3612eb1abc393d010273744f306419b2b9
JordanWilliford21 <jordanwilliford@westernwyoming.edu>
2025-03-28 08:30:44 +0000

    Ready to grade

--- name-and-address/main.py
+++ name-and-address/main.py
@@ -10,13 +10,52 @@
 
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Name and Address")
-window.geometry("300x300")
+class MyGUI:
+    def __init__(self):
+        # Create the main window
+        self.main_window = tk.Tk()
 
-hello = tk.Label(text="Hello world!")
-hello.pack()
-button = tk.Button(text="Click me!")
-button.pack()
+        # Create StringVar objects to display name,
+        # street, and city-state-zip
+        self.name_value = tk.StringVar()
+        self.street_value = tk.StringVar()
+        self.csz_value = tk.StringVar()
 
-tk.mainloop()
+        # Create two frames
+        self.info_frame = tk.Frame(self.main_window)
+        self.button_frame = tk.Frame(self.main_window)
+
+        # Create the label widgets, associated with the StringVar objects
+        self.name_label = tk.Label(self.info_frame, textvariable=self.name_value)
+        self.street_label = tk.Label(self.info_frame, textvariable=self.street_value)
+        self.csz_label = tk.Label(self.info_frame, textvariable=self.csz_value)
+
+        # Pack the labels
+        self.name_label.pack()
+        self.street_label.pack()
+        self.csz_label.pack()
+
+        # Create the button widgets
+        self.show_info_button = tk.Button(self.button_frame, text='Show Info', command=self.show)
+        self.quit_button = tk.Button(self.button_frame, text='Quit', command=self.main_window.destroy)
+
+        # Pack the buttons
+        self.show_info_button.pack(side='left')
+        self.quit_button.pack(side='right')
+
+        # Pack the frames
+        self.info_frame.pack()
+        self.button_frame.pack()
+
+        # Enter the tkinter main loop
+        self.main_window.mainloop()
+
+    # Callback function for the show info button
+    def show(self):
+        self.name_value.set('Jordan Williford')
+        self.street_value.set('1650 Stella CT')
+        self.csz_value.set('Cody, Wyoming 82414')
+
+
+# Creating instance of MyGUI class
+my_gui = MyGUI()
